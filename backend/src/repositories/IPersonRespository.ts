4b3783e1af6aa1fba19d6b55cf0a820ead6d885e
Mauricio <mauriciobarboza6.1@gmail.com>
2022-04-08 17:26:10 -0300

    Adicionados módulos de deleção e leitura de pessoas

--- backend/src/repositories/IPersonRespository.ts
+++ backend/src/repositories/IPersonRespository.ts
@@ -1,7 +1,14 @@
 import { Person } from './../entities/Person';
 
 export interface IPersonRepository{
-    deleteAllPersons(): Promise<void>
     save(person: Person): Promise<Person>
+    createMany(persons: Array<Person>): Promise<void>
+
     checkIfExists(cpf: string): Promise<Boolean>
+    findById(id: number): Promise<Person | null>
+    findAll(): Promise<Array<Person>>
+    findByName(name: string): Promise<Array<Person>>
+    
+    deleteAllPersons(): Promise<void>
+    deletePerson(id: number): Promise<Person>
 }
