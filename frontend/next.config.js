624b13d9217f8eb5f34559158dad651efe6dbce2
Mauricio <mauriciobarboza6.1@gmail.com>
2022-04-14 07:41:29 -0300

    Habilitado pooling para container next

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: false
-}
-
-module.exports = nextConfig
+module.exports = {
+  webpackDevMiddleware: (config) => {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
+  }
+};
